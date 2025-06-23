0b4e2f2566c85ed67396fe18c8c99ae1fa6d319f
gdar463 <gdar463@gmail.com>
2025-06-23 13:09:07 +0200

    commented image.py

--- movenet/image.py
+++ movenet/image.py
@@ -1,7 +1,9 @@
 import cv2
 
+# Threshold for accepting predictions
 keypoints_threshold = 0.11
 
+# For understanding which joint (not used ATM)
 KEYPOINT_DICT = {
     'nose': 0,
     'left_eye': 1,
@@ -22,6 +24,7 @@ KEYPOINT_DICT = {
     'right_ankle': 16
 }
 
+# BGR map for OpenCV
 COLOR_MAP = {
     'm': (255, 0, 255),   # magenta
     'c': (255, 255, 0),   # cyan
@@ -30,6 +33,7 @@ COLOR_MAP = {
     'red': (147, 20, 255)
 }
 
+# Color map for keypoints
 KEYPOINT_EDGE_INDS_TO_COLOR = {
     (0, 1): 'm',
     (0, 2): 'c',
@@ -54,35 +58,38 @@ KEYPOINT_EDGE_INDS_TO_COLOR = {
 
 # noinspection PyUnresolvedReferences,PyUnboundLocalVariable
 def draw_prediction(image, keypoints, crop_region=None, output_image_height=None):
-  img = image.copy()
-  height, width = img.shape[:2]
+  height, width = image.shape[:2]
 
+  # Draw model output
   num_instances, _, _, _ = keypoints.shape
   for i in range(num_instances):
     kpts_x = keypoints[0, i, :, 1] * width
     kpts_y = keypoints[0, i, :, 0] * height
     kpts_scores = keypoints[0, i, :, 2]
 
+    # Draw edges
     for (p1, p2), color_key in KEYPOINT_EDGE_INDS_TO_COLOR.items():
-
       if kpts_scores[p1] > keypoints_threshold and kpts_scores[p2] > keypoints_threshold:
         pt1 = (int(kpts_x[p1]), int(kpts_y[p1]))
         pt2 = (int(kpts_x[p2]), int(kpts_y[p2]))
-        cv2.line(img, pt1, pt2, COLOR_MAP[color_key], 3)
+        cv2.line(image, pt1, pt2, COLOR_MAP[color_key], 3)
 
+    # Draw keypoints (joints)
     for x, y, score in zip(kpts_x, kpts_y, kpts_scores):
       if score > keypoints_threshold:
-        cv2.circle(img, (int(x), int(y)), 6, COLOR_MAP['red'], -1)
+        cv2.circle(image, (int(x), int(y)), 6, COLOR_MAP['red'], -1)
 
+  # Crop (if asked, for video)
   if crop_region is not None:
     xmin = int(max(crop_region['x_min'] * width, 0.0))
     ymin = int(max(crop_region['y_min'] * height, 0.0))
     xmax = int(min(crop_region['x_max'], 0.99) * width)
     ymax = int(min(crop_region['y_max'], 0.99) * height)
-    cv2.rectangle(img, (xmin, ymin), (xmax, ymax), COLOR_MAP['b'], 1)
+    cv2.rectangle(image, (xmin, ymin), (xmax, ymax), COLOR_MAP['b'], 1)
 
+  # Resize (if asked)
   if output_image_height is not None:
     output_image_width = int(output_image_height * width / height)
-    img = cv2.resize(img, (output_image_width, output_image_height), interpolation=cv2.INTER_CUBIC)
+    image = cv2.resize(image, (output_image_width, output_image_height), interpolation=cv2.INTER_CUBIC)
 
-  return img
+  return image
