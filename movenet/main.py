4474e44b4a76a4e4ec660004251307deecb715b1
gdar463 <gdar463@gmail.com>
2025-06-24 18:08:07 +0200

    removed unnecessary timing force end call

--- movenet/main.py
+++ movenet/main.py
@@ -9,7 +9,6 @@ import numpy as np
 try:
   with Timing("TF Load") as t:
     import tensorflow as tf
-    t.force_end()
 except ImportError:
   sys.exit("TensorFlow is not installed. Please install it and try again.\n"
            "Check the TF wiki for information (https://www.tensorflow.org/install/pip)")
