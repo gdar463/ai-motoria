257b309baad9f6f46333b23da812d9908dc41259
gdar463 <gdar463@gmail.com>
2025-06-24 18:05:53 +0200

    fixed timing class not being returned

--- movenet/timing.py
+++ movenet/timing.py
@@ -5,11 +5,11 @@ import os
 class Timing:
   def __init__( self, message ):
     self.message = message
-    pass
 
   def __enter__( self ):
     print(f"started timing {self.message}")
     self.start = time.time()
+    return self
 
   def __exit__( self, exc_type, exc_value, traceback ):
     self.end = time.time()
