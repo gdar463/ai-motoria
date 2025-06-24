aeddbecc1e6ecb97917679863486414bc4a2b478
gdar463 <gdar463@gmail.com>
2025-06-24 18:00:56 +0200

    changed timing messages

--- movenet/timing.py
+++ movenet/timing.py
@@ -8,9 +8,10 @@ class Timing:
     pass
 
   def __enter__( self ):
+    print(f"started timing {self.message}")
     self.start = time.time()
 
   def __exit__( self, exc_type, exc_value, traceback ):
     self.end = time.time()
     if "DEBUG" in os.environ:
-      print(f"{self.message}: {self.end - self.start}")
+      print(f"{self.message} timing: {self.end - self.start}")
