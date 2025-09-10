58b53db0b9a9fba892852dd0ac140456201a4be5
gdar463 <gdar463@gmail.com>
2025-09-10 20:03:27 +0200

    added video radio buttons for ui

--- movenet/dialog.py
+++ movenet/dialog.py
@@ -2,13 +2,22 @@ import qtawesome as qta
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QDialog, QDialogButtonBox, QFileDialog, QLabel, QVBoxLayout
 
-from prediction import run_photo
+from prediction import run_photo, run_video
 
 
-def open_file_dialog( parent, output_path ):
-    filename = QFileDialog.getOpenFileName(parent, "Open File", "", "Images (*.jpg *.jpeg)", )
+def open_file_dialog_photo( parent, output_path ):
+    filename = QFileDialog.getOpenFileName(parent, "Open File", "",
+                                           "Images (*.jpg *.jpeg *.png *.bmp)", )
     if filename[0] != "":
         run_photo(filename[0], output_path)
+    else:
+        Dialog("Error", "<p style=\"font-size:13px\">No file selected</p>").exec()
+
+def open_file_dialog_video( parent, output_path ):
+    filename = QFileDialog.getOpenFileName(parent, "Open File", "",
+                                           "Videos (*.gif)", )
+    if filename[0] != "":
+        run_video(filename[0], output_path)
     else:
         Dialog("Error", "<p style=\"font-size:13px\">No file selected</p>").exec()
 
--- movenet/ui.py
+++ movenet/ui.py
@@ -4,16 +4,18 @@ import sys
 
 import qtawesome as qta
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QLabel, QLineEdit, QMainWindow, QPushButton, QSizePolicy, QVBoxLayout, \
+from PySide6.QtWidgets import QApplication, QButtonGroup, QLabel, QLineEdit, QMainWindow, QPushButton, \
+    QRadioButton, QSizePolicy, \
+    QVBoxLayout, \
     QWidget
 
-from dialog import open_file_dialog
+from dialog import Dialog, open_file_dialog_photo, open_file_dialog_video
 
 
 class MainWindow(QMainWindow):
     def __init__( self ):
         super().__init__()
-        self.setFixedSize(200, 100)
+        self.setFixedSize(300, 190)
 
         self.setWindowTitle("Movenet")
         self.setWindowIcon(qta.icon("mdi.dumbbell", color="white"))
@@ -24,28 +26,65 @@ class MainWindow(QMainWindow):
 class MainWidget(QWidget):
     def __init__( self, parent ):
         super().__init__()
-        self.setFixedSize(200, 100)
+        self.setFixedSize(300, 170)
         self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
 
-        output_label = QLabel("<p style=\"font-size:16px\">Output File Name</p>")
-        output_label.setTextFormat(Qt.TextFormat.RichText)
-        self.output_name = QLineEdit()
-        self.output_name.setPlaceholderText("es. output.png")
-        self.output_name.setText("output.png")
+        radio_label = QLabel("<p style=\"font-size:16px\">Select Input Type</p>")
+        radio_label.setTextFormat(Qt.TextFormat.RichText)
+
+        image_radio = QRadioButton()
+        image_radio.setText("Image")
+        image_radio.setStyleSheet("font-size: 12px")
+        image_radio.setChecked(True)
+        video_radio = QRadioButton()
+        video_radio.setText("Video")
+        video_radio.setStyleSheet("font-size: 12px")
+
+        radio_group = QButtonGroup()
+        radio_group.addButton(image_radio)
+        radio_group.addButton(video_radio)
+        self.radio_group = radio_group
+        radio_layout = QVBoxLayout()
+        radio_layout.addWidget(radio_label)
+        radio_layout.addWidget(image_radio)
+        radio_layout.addWidget(video_radio)
+        radio_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
         run_button = QPushButton()
         run_button.setText("Run Prediction")
         run_button.setStyleSheet("font-size: 14px")
         run_button.clicked.connect(self.run_prediction)
+        run_button.setDisabled(True)
+        self.run_button = run_button
+
+        output_label = QLabel("<p style=\"font-size:16px\">Output File Name</p>")
+        output_label.setTextFormat(Qt.TextFormat.RichText)
+        output_name = QLineEdit()
+        output_name.setPlaceholderText("es. output.png")
+        output_name.textChanged.connect(self.text_changed)
+        self.output_name = output_name
 
         layout = QVBoxLayout()
+        layout.addLayout(radio_layout)
         layout.addWidget(output_label, alignment=Qt.AlignmentFlag.AlignHCenter)
         layout.addWidget(self.output_name, alignment=Qt.AlignmentFlag.AlignHCenter)
         layout.addWidget(run_button, alignment=Qt.AlignmentFlag.AlignHCenter)
         self.setLayout(layout)
 
     def run_prediction( self ):
-        open_file_dialog(self, self.output_name.text())
+        if self.radio_group.checkedButton().text() == "Image":
+            open_file_dialog_photo(self, self.output_name.text())
+        else:
+            if not self.output_name.text().endswith(".mp4"):
+                Dialog("Error", "<p style=\"font-size:13px\">Output file name not valid. Only mp4 files allowed</p>").exec()
+            else:
+                open_file_dialog_video(self, self.output_name.text())
+
+    def text_changed( self ):
+        if self.output_name.text() != "":
+            self.run_button.setDisabled(False)
+        else:
+            self.run_button.setDisabled(True)
 
 
 if __name__ == "__main__":
